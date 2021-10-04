aa6fcbf2b39aea006987dcb6bf69f68c11614dca
Kate <pink-angel@mail.ru>
2021-10-04 17:04:42 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.sql.SQLOutput;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketCost = 4_999;
+        int mile = 20;
+        int bonusMiles = ticketCost / mile;
+        System.out.println(bonusMiles);
+
+    }
+
+
+}
